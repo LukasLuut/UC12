eae899898c982088cca1769d25486c2de3df1f27
lukasluut <lukasluut@gmail.com>
2025-07-08 17:23:10 -0300

    trabalho com CSS e animações

--- 02-07/Exercício/script.js
+++ 02-07/Exercício/script.js
@@ -1,10 +1,19 @@
 //Declaração de variáveis 
 const nav = document.getElementById('nav')
 const logo = document.getElementById('logo')
+const header = document.querySelector('header')
 
 
 //Lógica
-logo.addEventListener('click', (event)=>{
+logo.addEventListener('click', (event) => {
     nav.classList.toggle('hide-nav')
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('change')
+    } else {
+        header.classList.remove('change')
+    }
 })
 
