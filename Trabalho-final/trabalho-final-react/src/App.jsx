cc551ae802ed43413c81a6dec671b16f4381a7cf
lukasluut <lukasluut@gmail.com>
2025-07-24 17:25:59 -0300

    trabalho final

--- Trabalho-final/trabalho-final-react/src/App.jsx
+++ Trabalho-final/trabalho-final-react/src/App.jsx
@@ -6,27 +6,36 @@ import Banner from './components/Banner'
 
 
 function App() {
- 
+
 
   return (
     <>
-      <Header/>
-        <main>
-          <Card/>
-            <section className='recommended-destination-div'>
-              <div className='recommended-destination-div2'>
-                <h2>Recommended Destination</h2>
-                <Button2 texto="View More"></Button2>
-              </div>
-              <div className='recommended-destination-div3'>
-                <Banner></Banner>
-                <Banner></Banner>
-                <Banner></Banner>
-              </div>
-            </section>
-         
-        </main>
-      
+      <Header />
+      <main>
+        <Card />
+        <section className='recommended-destination-div'>
+          <div className='recommended-destination-div2'>
+            <h2>Recommended Destination</h2>
+            <Button2 texto="View More"></Button2>
+          </div>
+          <div className='banners'>
+            <Banner></Banner>
+            <Banner></Banner>
+            <Banner></Banner>
+          </div>
+        </section>
+        <section className='section2'>
+          <div className='div-section2 section2'>
+            <h2>Elevate Your Epic <br></br> Adventures</h2>
+            <p>We are a travel agency that specializes <br></br> in customizing trips according to your <br></br> preferences, needs, and desires</p>
+          </div>
+        </section>
+        <section className='section3'>
+          
+          <img src="https://live.sociolib.com/nomad/wp-content/uploads/sites/15/2025/07/exotic-tropical-seascape-with-palm-tree-jetty-pie-2024-10-18-08-01-58-utc.webp" alt="" />
+        </section>
+      </main>
+
     </>
   )
 }
--- Trabalho-final/trabalho-final-react/src/components/Banner.jsx
+++ Trabalho-final/trabalho-final-react/src/components/Banner.jsx
@@ -6,17 +6,33 @@ import Button2 from './Button2'
 function Banner() {
   return (
     <div className='div-banner'>
+        <div className='div-country'>
+          <img src="https://s.w.org/images/core/emoji/16.0.1/svg/1f1f0-1f1ff.svg" alt="bandeira do Kazakhstan" />
+          <p>Kazakhstan</p>
+        </div>
         <img src="https://live.sociolib.com/nomad/wp-content/uploads/sites/15/2025/07/colin-watts-M1ObxvsWVhY.webp" alt="" />
         <div>   
             <h3>Caspian Sea Beach, Aktau</h3>
-            <p>A Unique Seaside Escape in Central Asia</p>
+            <p className='desc-destination'>A Unique Seaside Escape in Central Asia</p>
             <div className='detail'>
-                <p> 12 jun</p>
-                <p> 5.0 Rating </p>
-                <p> Bitch </p>
+                <div className='p-icon'>
+                  <img src="src/img/calendario.png" alt="ícone de um calendário" />
+                  <p> 12 jun</p>
+                </div>
+                <p>|</p>
+                <div className='p-icon'>
+                  <img src="src/img/estrela.png" alt="" />
+                  <p> 5.0 Rating </p>                  
+                </div>
+                <p>|</p>
+                <div className='p-icon'>
+                  <img src="src/img/guarda-sol.png" alt="" />
+                  <p> Bitch </p>
+                </div>
+                
             </div>
             <div className='values'>
-              <p>$2.550 <br />7 days package</p>
+              <p>$2.550 <br /><span>7 days package</span></p>
               <Button2 texto='Book Now'></Button2>
             </div>
         </div>
